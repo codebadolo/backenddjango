283f7c263dd5e24491e093503f81c170da4e4d62
codebadolo <codewithbadolo@gmail.com>
2023-11-08 13:29:01 +0000

    before  embassy

--- mainwoulou/social/urls.py
+++ mainwoulou/social/urls.py
@@ -3,7 +3,7 @@ from django.urls import re_path
 from .views  import FacebookLogin
 
 urlpatterns = [
-  
- re_path(r'^rest-auth/facebook/$', FacebookLogin.as_view(), name='fb_login')
+ 
+    re_path(r'^rest-auth/facebook/$', FacebookLogin.as_view(), name='fb_login')
 ]
-#http://127.0.0.1:8000/social/rest-auth/facebook/
+#http://127.0.0.1:8000/social/rest-auth/facebook/https://www.facebook.com/reel/297284649808696
